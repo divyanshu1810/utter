39652ce0d19726978fa9dd2c6cfc07354f48ba38
Divyanshu Yadav <divyanshu.1810@gmaiil.com>
2023-03-11 11:07:42 +0530

    :lock: Auth Added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
+import Login from "@/components/Login";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { data: session } = useSession();
+  if (!session) return <Login />;
   return (
     <>
       <Head>
